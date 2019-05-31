7149960bce936a349061d15006c0a4c595b0a669
alexandre <ale156ale@hotmail.com>
2019-05-31 18:32:24 -0300

    outro commit

--- src/Handler/Emprestimo.hs
+++ src/Handler/Emprestimo.hs
@@ -8,15 +8,14 @@ module Handler.Emprestimo where
 
 import Import
 import Database.Persist.Postgresql
+import Database.Persist
 import Database.Persist.Sql
 
 getTodosEmprestimoR :: Handler Html
 getTodosEmprestimoR = do
-    --let sql = "SELECT * FROM \"livro\" LEFT JOIN \"emprestimo\" ON l.id = e.livid WHERE e.data_emp IS NULL;"
+    let sql = "SELECT * FROM \"livro\" LEFT JOIN \"emprestimo\" ON \"livro\".id = \"emprestimo\".livid \
+            \WHERE \"emprestimo\".data_emp IS NULL;"
     livros <- runDB $ rawSql sql []
     defaultLayout $(whamletFile "templates/emprestimos.hamlet")
-        where sql = "SELECT id \
-            \FROM livro \
-            \LEFT JOIN emprestimo ON livro.id=emprestimo.livid \
-            \WHERE emprestimo.data_emp IS NULL;"
+        -- where sql = "SELECT * FROM livro LEFT JOIN emprestimo ON livro.id=emprestimo.livid WHERE emprestimo.data_emp IS NULL;"
 

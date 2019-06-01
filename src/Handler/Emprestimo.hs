8b79cce93828e28dae3bff4160a428e64a15e5d7
alexandre <ale156ale@hotmail.com>
2019-06-01 09:15:53 -0300

    outro commit

--- src/Handler/Emprestimo.hs
+++ src/Handler/Emprestimo.hs
@@ -11,6 +11,18 @@ import Database.Persist.Postgresql
 import Database.Persist
 import Database.Persist.Sql
 
+formEmpr :: LivroId -> Form Emprestimo
+formEmpr livid = renderBootstrap $ Emprestimo
+    <$> areq (selectField cliLista) "Cliente: " Nothing
+    <*> pure livid
+    <*> areq dayField "Data Emprestimo: " Nothing
+    <*> areq dayField "Data Devolucao: " Nothing
+
+cliLista = do
+    entidades <- runDB $ selectList [] [Asc ClienteNome]
+    optionsPairs $ fmap (\ent -> (clienteNome $ entityVal ent, entityKey ent)) entidades
+
+
 getTodosEmprestimoR :: Handler Html
 getTodosEmprestimoR = do
     let sql = "SELECT ?? FROM livro LEFT JOIN emprestimo ON livro.id=emprestimo.livid \
@@ -20,16 +32,29 @@ getTodosEmprestimoR = do
 
 getEmprestarClienteR :: LivroId -> Handler Html
 getEmprestarClienteR livid = do
+    runDB $ get404 livid
+    msg <- getMessage
+    (widget,enctype) <- generateFormPost (formEmpr livid)
     defaultLayout $ do
+        addStylesheet $ StaticR css_bootstrap_css
         [whamlet|
-            Oi GET
+            $maybe mensagem <- msg
+                ^{mensagem}
+            <form action=@{EmprestarClienteR livid} method=post enctype=#{enctype}>
+                ^{widget}
+                <input type="submit" value="Cadastrar">
         |]
 
 postEmprestarClienteR :: LivroId -> Handler Html
 postEmprestarClienteR livid = do
-    defaultLayout $ do
-        [whamlet|
-            Oi POST
-        |]
+    ((res,_),_) <- runFormPost (formEmpr livid)
+    case res of
+        FormSuccess empr -> do
+            _ <- runDB $ insert empr
+            setMessage [shamlet|
+                 <h3> Emprestimo cadastrado com sucesso
+            |]
+            redirect (EmprestarClienteR livid)
+        _ -> redirect HomeR
 
 

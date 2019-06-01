71dff7d79aaeb14c37c5dcc315d02921fd8d43c8
alexandre <ale156ale@hotmail.com>
2019-06-01 16:17:57 -0300

    outro commit

--- src/Handler/Home.hs
+++ src/Handler/Home.hs
@@ -16,44 +16,44 @@ getHomeR = do
         addStylesheet $ StaticR css_bootstrap_css
         [whamlet|
             $maybe sessao <- sess
-                <h3>Gestão de bibliotecas
-                Ola #{sessao}
+                <h4><center><strong>Gestão de bibliotecas
+                <h5><center><strong>Ola #{sessao}
             $nothing
                 <h3>Gestão de bibliotecas
-            <ul>
-                    <br><h4>------- Funcionário ---------</h4>
-                <li>
-                    <a href=@{FuncionarioR}>
-                        Cadastro de Funcionario
-                <li>
-                    <a href=@{TodosFuncionariosR}>
-                        Listar Funcionarios
-                    <h4>------- Clientes ---------</h4>
-                <li>
-                    <a href=@{ClienteR}>
-                        Cadastro de Cliente
-                <li>
-                    <a href=@{TodosClientesR}>
-                        Listar Clientes
-                    <h4>------- Livros ---------</h4>
-                <li>
-                    <a href=@{LivroR}>
-                        Cadastro de Livro
-                <li>
-                    <a href=@{TodosLivrosR}>
-                        Listar Livros
-                    <h4>------ Emprestimos/Devolucoes</h4>
-                <li>
-                    <a href=@{TodosEmprestimoR}>
-                        Emprestar Livros
-                <li>
-                    <a href=@{TodosDevolucaoR}>
-                        Devolucao de Livros
-                   <h4>------- Login/Logout -------</h4>
+            <ul class="list-group">
+                    <br><center><h4>------- Funcionário ---------</h4>
+                <li class="list-group-item">
+                    <center><a href=@{FuncionarioR}>
+                        <center>Cadastro de Funcionario
+                <li class="list-group-item">
+                    <center><a href=@{TodosFuncionariosR}>
+                        <center>Listar Funcionarios
+                    <center><h4>------- Clientes ---------</h4>
+                <li class="list-group-item">
+                    <center><a href=@{ClienteR}>
+                        <center>Cadastro de Cliente
+                <li class="list-group-item">
+                    <center><a href=@{TodosClientesR}>
+                        <center>Listar Clientes
+                    <center><h4>------- Livros ---------</h4>
+                <li class="list-group-item">
+                    <center><a href=@{LivroR}>
+                        <center>Cadastro de Livro
+                <li class="list-group-item">
+                    <center><a href=@{TodosLivrosR}>
+                        <center>Listar Livros
+                    <center><h4>------ Emprestimos/Devolucoes</h4>
+                <li class="list-group-item">
+                    <center><a href=@{TodosEmprestimoR}>
+                        <center>Emprestar Livros
+                <li class="list-group-item">
+                    <center><a href=@{TodosDevolucaoR}>
+                        <center>Devolucao de Livros
+                   <center><h4>------- Login/Logout -------</h4>
                 $maybe _ <- sess
                     <form action=@{LogoutR} method=post>
                         <input type="submit" value="Sair">
                 $nothing
-                    <a href=@{LoginR}>
+                    <center><a href=@{LoginR}>
                         Entrar
         |] 

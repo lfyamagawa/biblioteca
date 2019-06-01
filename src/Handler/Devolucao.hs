44ef3129fb3016a4d3db3adb8aff1093c21efca5
alexandre <ale156ale@hotmail.com>
2019-06-01 14:23:21 -0300

    outro commit

--- src/Handler/Devolucao.hs
+++ src/Handler/Devolucao.hs
@@ -18,11 +18,10 @@ getTodosDevolucaoR = do
     --    \INNER JOIN cliente ON emprestimo.cliid=cliente.id \
     --    \INNER JOIN livro ON emprestimo.livid=livro.id \
     --    \WHERE emprestimo.data_emp IS NOT NULL;"
-    let sql_ = "SELECT ??,?? FROM emprestimo \
+    let sql_ = "SELECT ?? FROM emprestimo \
         \INNER JOIN cliente ON emprestimo.cliid=cliente.id \
         \INNER JOIN livro ON emprestimo.livid=livro.id \
         \WHERE emprestimo.data_emp IS NOT NULL;"
- 
     devolucoes <- runDB $ rawSql sql_ []
     defaultLayout $(whamletFile "templates/devolucoes.hamlet")
 

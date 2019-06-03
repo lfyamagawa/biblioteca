2bf9299975999f7954ab129616d82a178b68e604
lfyamagawa <37278077+lfyamagawa@users.noreply.github.com>
2019-06-03 20:06:54 -0300

    Página Home Configurada e Pronta

--- src/Handler/Home.hs
+++ src/Handler/Home.hs
@@ -15,45 +15,99 @@ getHomeR = do
     defaultLayout $ do
         addStylesheet $ StaticR css_bootstrap_css
         [whamlet|
+
+        <div class="container text-center" style="background-color:black">
             $maybe sessao <- sess
-                <h2><center><strong>Gestão de bibliotecas
-                <h3><center><strong>Ola #{sessao}
+                <h1><strong><font color="white">GESTÃO DE BIBLIOTECAS
+                <br>
+                <font color="white"><p class="text-right">Olá: #{sessao}
+                <table>
+                    <tr>
+                       <td class="col col-sm-12">
+                           <strong><font color="white"><p class="text-right">Logout:  
+                       
+                       <td>
+                           <form action=@{LogoutR} method=post>
+                               <button class="btn btn-sm active" type="submit"  role="button" aria-pressed="true">
+                                  <font color="black">Sair                         
             $nothing
-                <h3>Gestão de bibliotecas
-            <ul class="list-group">
-                <li class="list-group-item">
-                    <br><center><h4>------- Funcionário ---------</h4>
-                    <center><a href=@{FuncionarioR}>
-                        <center>Cadastro de Funcionario
-                <li class="list-group-item">
-                    <center><a href=@{TodosFuncionariosR}>
-                        <center>Listar Funcionarios
-                    <center><h4>------- Clientes ---------</h4>
-                <li class="list-group-item">
-                    <center><a href=@{ClienteR}>
-                        <center>Cadastro de Cliente
-                <li class="list-group-item">
-                    <center><a href=@{TodosClientesR}>
-                        <center>Listar Clientes
-                    <center><h4>------- Livros ---------</h4>
-                <li class="list-group-item">
-                    <center><a href=@{LivroR}>
-                        <center>Cadastro de Livro
-                <li class="list-group-item">
-                    <center><a href=@{TodosLivrosR}>
-                        <center>Listar Livros
-                    <center><h4>------ Emprestimos/Devolucoes</h4>
-                <li class="list-group-item">
-                    <center><a href=@{TodosEmprestimoR}>
-                        <center>Emprestar Livros
-                <li class="list-group-item">
-                    <center><a href=@{TodosDevolucaoR}>
-                        <center>Devolucao de Livros
-                   <center><h4>------- Login/Logout -------</h4>
-                $maybe _ <- sess
-                    <form action=@{LogoutR} method=post>
-                        <input type="submit" value="Sair">
-                $nothing
-                    <center><a href=@{LoginR}>
-                        Entrar
+                <h1><strong><font color="white">GESTÃO DE BIBLIOTECAS
+                <br>
+         
+                <table>
+                    <tr>
+                       <td class="col col-sm-12">
+                           <strong><font color="white"><p class="text-right">Login:  
+                       <td>
+                           <a href=@{LoginR} class="btn btn-danger btn-sm active" type="submit"  role="button" aria-pressed="true">
+                              <strong><font color="white">Entrar 
+            <br>          
+
+       
+            
+        <table class="container text-center">
+            <thead style="background-color:black">
+                <tr>
+                   <br>
+                   <th>
+                      <center><h4><strong><font color="white">FUNCIONÁRIOS
+                   <th>
+                      <center><h4><strong><font color="white">CLIENTE
+                   
+            <tbody>
+                <tr>
+                   <td>
+                     <br>
+                        <a href=@{FuncionarioR}>
+                          <h5><strong><font color="black">Cadastro de Funcionario
+                   <td>
+                     <br>
+                        <a href=@{ClienteR}>
+                          <h5><strong><font color="black">Cadastro de Cliente
+                          
+                <tr>          
+                   <td>
+                        <a href=@{TodosFuncionariosR}>
+                          <h5><strong><font color="black">Listar Funcionarios
+                   <td>
+                        <a href=@{TodosClientesR}>
+                          <h5><strong><font color="black">Listar Clientes
+               
+        <table class="container text-center">
+            <thead style="background-color:black">
+                <tr>
+                   <br>
+                   <th>
+                      <center><h4><strong><font color="black">__________________<font color="white">LIVROS<font color="black">__________________
+                   <th>
+                      <center><h4><strong><font color="white">EMPRÉSTIMOS E DEVOLUÇÕES
+                   
+            <tbody>
+                <tr>
+                   <td>
+                     <br>
+                        <a href=@{LivroR}>
+                          <h5><strong><font color="black">Cadastro de Livros
+                   <td>
+                     <br>
+                        <a href=@{TodosEmprestimoR}>
+                          <h5><strong><font color="black">Empréstimo de Livro
+
+                <tr>          
+                   <td>
+                        <a href=@{TodosLivrosR}>
+                          <center><h5><strong><font color="black">Listar Livros
+                   <td>
+                        <a href=@{TodosDevolucaoR}>
+                          <center><h5><strong><font color="black">Devolução de Livros
+        
+        <br>                  
+
+
+        <section class="title-content container" style="background-image: url(https://live.staticflickr.com/65535/47993113741_f9a1f150c8_h.jpg")">
+                        <br>
+                        <center><font color="navy"><strong><h1 class="title-text">Biblioteca Virtual
+                        <br>
+        <br>                
+                                  
         |] 
